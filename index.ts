e14e5842cf918df95420436a81f4370e7f852a0d
nebulisx <nebulisx@gmail.com>
2025-01-02 08:45:14 +0200

    feat: implement command interface for task management
    Add a new script to handle task management commands via the CLI. 
    This includes functionality to add, list, delete, update, and 
    change the status of tasks. The implementation uses the `util` 
    module to parse command-line arguments and routes commands to 
    the appropriate functions in the `commands` module. This change 
    enhances user interaction with the task management system.

---
+++ index.ts
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+import { parseArgs } from "util"
+import * as db from "./lib/db"
+import { commands } from "./commands"
+
+const args = process.argv.slice(2);
+const options = {
+  "task": {
+    type: 'string'
+  },
+
+}
+
+const { values, positionals } = parseArgs({
+  args,
+  options: {
+    "task": {
+      type: 'string',
+    },
+    "status": {
+      type: 'string'
+    },
+    "id": {
+      type: 'string'
+    }
+  },
+  strict: true,
+  allowPositionals: true
+});
+
+switch (positionals[0]) {
+  case "add":
+    commands.add(positionals[1])
+    break;
+  case "list":
+    commands.list(positionals[1] as "done" | "pending" | "in-progress" | undefined)
+    break;
+  case "delete":
+    commands.remove(parseInt(positionals[1]))
+    break;
+  case "update":
+    commands.update(parseInt(values.id as string), values.task as string)
+    break;
+  case "mark-done":
+    commands["update-status"](parseInt(positionals[1]), "done")
+    break;
+  case "mark-pending":
+    commands["update-status"](parseInt(positionals[1]), "pending")
+    break;
+  case "mark-in-progress":
+    commands["update-status"](parseInt(positionals[1]), "in-progress")
+    break;
+  default:
+    console.log("Invalid command");
+}
