d0f1a6f3d1618327d671c2b21c024ae9cc06f376
nebulisx <nebulisx@gmail.com>
2025-01-02 09:28:44 +0200

    refactor: update task structure and management functions
    Refactor the task management system to include an ID, description, 
    createdAt, and updatedAt fields for each task. Change the ID to be a 
    string generated from the current date's day, minutes, and seconds. 
    Update the add, remove, updateStatus, and update functions to handle 
    the new task structure and ensure proper task retrieval and 
    modification. This improves task identification and tracking.

--- lib/db.ts
+++ lib/db.ts
@@ -1,41 +1,58 @@
-const tasksPath = "lib/tasks.json"
-const tasksFile = await Bun.file(tasksPath)
+const tasksPath = "lib/tasks.json";
+const tasksFile = await Bun.file(tasksPath);
 
 interface Task {
-  task: string;
+  id: string; // This should be a number, but it's a string in the JSON file. It Should be day + minutes + seconds
+  description: string;
   status: "pending" | "in-progress" | "done";
+  createdAt: string;
+  updatedAt: string;
 }
 
-const tasks: Task[] = await tasksFile.exists() ? await tasksFile.json() : []
+let tasks: Task[] = (await tasksFile.exists()) ? await tasksFile.json() : [];
 
 export const get = () => {
   return tasks;
 };
 
-export const add = async (task: string) => {
-  tasks.push({ task, status: "pending" });
+export const add = async (description: string) => {
+  tasks.push({
+    id: `${new Date().getDay()}${new Date().getMinutes()}${new Date().getSeconds()}`,
+    description,
+    status: "pending",
+    createdAt: new Date().toUTCString(),
+    updatedAt: new Date().toUTCString(),
+  });
   await saveTasks();
 };
 
-export const remove = async (id: number) => {
-  tasks.splice(id, 1);
+export const remove = async (id: string) => {
+  tasks = tasks.filter((task) => task.id !== id);
   await saveTasks();
 };
 
 export const updateStatus = async (
-  id: number,
+  id: string,
   status: "done" | "pending" | "in-progress"
 ) => {
-  const task = tasks[id];
+  const task = tasks.filter((task) => task.id === id)[0];
+  if (task === undefined) {
+    throw new Error("Task not found");
+  }
+
   task.status = status;
+  task.updatedAt = new Date().toUTCString();
   await saveTasks();
 };
 
-export const update = async (id: number, task: string) => {
-  if (tasks[id] == null) {
+export const update = async (id: string, description: string) => {
+  const task = tasks.filter((task) => task.id === id)[0];
+  if (task === undefined) {
     throw new Error("Task not found");
   }
-  tasks[id].task = task;
+
+  task.description = description;
+  task.updatedAt = new Date().toUTCString();
   await saveTasks();
 };
 
