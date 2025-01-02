33c7aede41fcd5a1803dd3b380f7ebdc8f67929a
nebulisx <nebulisx@gmail.com>
2025-01-02 17:57:47 +0200

    feat(commands): enhance task management with error handling
    Add error handling and user feedback for task operations. Implement checks 
    for empty inputs in `add` and `remove` commands, and provide console 
    messages for successful task additions and removals. Update the 
    `update-status` command to confirm status changes. These changes improve 
    user experience and prevent potential errors during task management.

--- commands/index.ts
+++ commands/index.ts
@@ -1,7 +1,20 @@
 import * as db from "../lib/db";
 
 export const commands = {
-  add: db.add,
+  add: (...descriptions: string[]) => {
+    if (descriptions.length === 0) {
+      console.log("No description provided");
+      return;
+    } else {
+      descriptions.forEach((description) => {
+        db.add(description)
+          .then((id) => console.log(`The task with id ${id} is added`))
+          .catch((error) => {
+            console.error(error.message);
+          });
+      });
+    }
+  },
   list: (status?: "done" | "pending" | "in-progress") => {
     if (status) {
       const tasks = db.get().filter((task) => task.status === status);
@@ -14,20 +27,47 @@ export const commands = {
         console.log(`${task.id}: ${task.description}`);
       });
     } else {
+      if (db.get().length === 0) {
+        console.log("No tasks found");
+        return;
+      }
       db.get().forEach((task) => {
         console.log(`${task.id}: ${task.description}`);
       });
     }
   },
-  remove: db.remove,
+  remove: (...ids: string[]) => {
+    if (ids.length === 0) {
+      console.log("No ids provided");
+      return;
+    }
+
+    if (ids.length === 1 && ids[0] === "all") {
+      db.get().map((task) =>
+        db
+          .remove(task.id)
+          .then(() => console.log(`The task with id ${task.id} is removed`))
+      );
+    }
+
+    ids.forEach((id) => {
+      db.remove(id)
+        .then(() => console.log(`The task with id ${id} is removed`))
+        .catch((error) => {
+          console.error(error.message);
+        });
+    });
+  },
   update: (id: string, description: string) => {
     db.update(id, description).catch((error) => {
       console.error(error.message);
     });
   },
   "update-status": (id: string, status: "done" | "pending" | "in-progress") => {
-    db.updateStatus(id, status).catch((error) => {
-      console.error(error.message);
-    });
+    db.updateStatus(id, status)
+      .then(() => console.log(`Marked task with id ${id} as ${status}`))
+      .catch((error) => {
+        console.error(error.message);
+      });
   },
 };
